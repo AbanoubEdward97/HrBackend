02600525a4aae1f03effe49a70e48c7f6964caa9
Abanoub Edward <pipo.edward@yahoo.com>
2025-11-23 16:03:23 +0200

    Initial commit - HR API project

---
+++ Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace HrApi.Models;
+public class Employee
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
---
+++ Models/HrContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+namespace HrApi.Models;
+public class HrContext : DbContext
+{
+    public HrContext(DbContextOptions<HrContext> options)  : base(options)
+    {
+        
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
